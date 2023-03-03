eca1bd8946a947e9586fba8810969fc81c45c3f6
Hypersus <Hypersus@outlook.com>
2023-03-03 19:48:29 +0800

    feat: viper support

--- main.go
+++ main.go
@@ -6,24 +6,28 @@ import (
 
 	"github.com/Hypersus/simplebank/api"
 	db "github.com/Hypersus/simplebank/db/sqlc"
+	"github.com/Hypersus/simplebank/util"
 	_ "github.com/lib/pq"
 )
 
-const (
-	dbDriver      = "postgres"
-	dbSource      = "postgres://root:hypersus@localhost:5432/simple_bank?sslmode=disable"
-	serverAddress = "localhost:8080"
-)
+// const (
+// 	dbDriver      = "postgres"
+// 	dbSource      = "postgres://root:hypersus@localhost:5432/simple_bank?sslmode=disable"
+// 	serverAddress = "localhost:8080"
+// )
 
 func main() {
-
-	sqlDB, err := sql.Open(dbDriver, dbSource)
+	config, err := util.LoadConfig(".")
+	if err != nil {
+		log.Fatal("config: cannot load config", err)
+	}
+	sqlDB, err := sql.Open(config.DBDriver, config.DBSource)
 	if err != nil {
 		log.Fatal("database: cannot connect to database", err)
 	}
 	store := db.NewStore(sqlDB)
 	server := api.NewServer(store)
-	err = server.Start(serverAddress)
+	err = server.Start(config.ServerAddress)
 	if err != nil {
 		log.Fatal("server: cannot start server", err)
 	}
