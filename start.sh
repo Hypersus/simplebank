2ef6db93851646a635c103e8b6c80418aa2336c4
Hypersus <Hypersus@outlook.com>
2023-05-26 20:46:08 +0800

    fix: set up env var for docker images

--- start.sh
+++ start.sh
@@ -5,6 +5,8 @@
 set -e
 
 echo "migrating database"
+source /app/app.env
+export DB_SOURCE="postgres://postgres:aA5!bB4#bK0!pU4#@192.168.0.212:5432/simple_bank"
 echo "DB_SOURCE: ${DB_SOURCE}"
 /app/migrate -path /app/migration -database ${DB_SOURCE} --verbose up
 echo "migrated database"
